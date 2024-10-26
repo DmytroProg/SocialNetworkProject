f507b6627c146344a5fcd0b2ff8267819e04cfd7
DevXCoder47 <nbelokrinitskiy@ukr.net>
2024-10-26 23:46:36 +0300

    Refactored models: User, Post

--- SocialNetwork/SocialNetwork.Core/Models/Post.cs
+++ SocialNetwork/SocialNetwork.Core/Models/Post.cs
@@ -4,9 +4,7 @@
     {
         public int Id { get; set; }
         public string? Description { get; set; }
-        public object? Media { get; set; }
+        public string? Media { get; set; }
         public long LikesCount { get; set; }
-        public long DislikesCount { get; set; }
-        public long RepostsCount { get; set; }
     }
 }
