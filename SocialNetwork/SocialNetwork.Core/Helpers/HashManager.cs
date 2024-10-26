60c280a5e2097c6a0cdad20acd2bab77b916b61f
Scrolix1591 <scrolix36@gmail.com>
2024-10-26 22:30:28 +0200

    HashManager refactored

--- SocialNetwork/SocialNetwork.Core/Helpers/HashManager.cs
+++ SocialNetwork/SocialNetwork.Core/Helpers/HashManager.cs
@@ -1,32 +1,32 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 
-namespace SocialNetwork.Core.Extensions
+namespace SocialNetwork.Core.Helpers
 {
     public static class HashManager
     {
         private static string GenerateHash(byte[] dataBytes)
         {
-            StringBuilder temp = new StringBuilder();
+            var stringBuilder = new StringBuilder();
 
-            using (SHA256 sha256 = SHA256.Create())
+            using (var sha256 = SHA256.Create())
             {
-                byte[] hashBytes = sha256.ComputeHash(dataBytes);
+                var hashBytes = sha256.ComputeHash(dataBytes);
 
                 for (int i = 0; i < hashBytes.Length; i++)
                 {
-                    temp.Append(hashBytes[i].ToString("x2"));
+                    stringBuilder.Append(hashBytes[i].ToString("x2"));
                 }
 
-                return temp.ToString().Substring(0, 16);
+                return stringBuilder.ToString().Substring(0, 16);
             }
         }
 
         public static string HashCreate(object data)
         {
-            string salt = DateTime.UtcNow.ToString("yyyyMMddHHmmssffff");
+            var salt = DateTime.UtcNow.ToString("yyyyMMddHHmmssffff");
 
-            byte[] dataBytes = Encoding.UTF8.GetBytes(data.ToString() + salt);
+            var dataBytes = Encoding.UTF8.GetBytes(data.ToString() + salt);
 
             return GenerateHash(dataBytes);
         }
@@ -35,7 +35,7 @@ namespace SocialNetwork.Core.Extensions
 
         private static string RecoverHash(object data, DateTime dateTime)
         {
-            byte[] dataBytes = Encoding.UTF8.GetBytes(data.ToString() + dateTime);
+            var dataBytes = Encoding.UTF8.GetBytes(data.ToString() + dateTime);
 
             return GenerateHash(dataBytes);
         }
@@ -44,7 +44,7 @@ namespace SocialNetwork.Core.Extensions
         public static bool HashCompare(object data, DateTime oldDateTime, string oldHash)
         {
             //data - password, dateTime - date and time of creating account, oldHasg - saved in db hash of pass or etc.
-            string result = RecoverHash(data, oldDateTime);
+            var result = RecoverHash(data, oldDateTime);
 
             return result == oldHash;
         }
