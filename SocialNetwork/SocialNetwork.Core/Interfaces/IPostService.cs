555ca5cbd7e67f38876bae4469d1a5766adb053f
DevXCoder47 <nbelokrinitskiy@ukr.net>
2024-10-28 15:51:37 +0200

    PostService created

---
+++ SocialNetwork/SocialNetwork.Core/Interfaces/IPostService.cs
@@ -0,0 +1,17 @@
+﻿using SocialNetwork.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Core.Interfaces
+{
+    public interface IPostService
+    {
+        public List<Post> Posts { get; set; }
+        public void CreatePost(Post post);
+        public List<Post> GetPosts();
+        public Post? GetPostById(int id);
+    }
+}
---
+++ SocialNetwork/SocialNetwork.Core/Services/PostService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SocialNetwork.Core.Interfaces;
+using SocialNetwork.Core.Models;
+namespace SocialNetwork.Core.Services
+{
+    public class PostService : IPostService
+    {
+        // TODO repository
+        public List<Post> Posts { get; set; }
+        public PostService()
+        {
+            Posts = new List<Post>();
+        }
+        public void CreatePost(Post post)
+        {
+            if (post.Description == null || post.LikesCount < 0)
+                throw new Exception("Invalid post build");  // TODO own types of exceptions
+            Posts.Add(post);
+        }
+        public Post? GetPostById(int id)
+        {
+            Post? post = Posts.FirstOrDefault(unit => unit.Id == id);
+            return post;
+        }
+        public List<Post> GetPosts()
+        {
+            return Posts;
+        }
+    }
+}
