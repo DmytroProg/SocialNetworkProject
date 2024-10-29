bce4024cacd71ee908839d50598c6a778d2d50fb
DevXCoder47 <nbelokrinitskiy@ukr.net>
2024-10-29 16:05:11 +0200

    UserService refactored

--- SocialNetwork/SocialNetwork.Core/Interfaces/IUserService.cs
+++ SocialNetwork/SocialNetwork.Core/Interfaces/IUserService.cs
@@ -2,12 +2,11 @@
 namespace SocialNetwork.Core.Interfaces;
 public interface IUserService
 {
-    List<User> Users { get; set; }
-    List<User> GetUsers();
+    IEnumerable<User> GetUsers();
     User? GetUserById(int id);
     User? GetUserByName(string name);
     void UpdateUser(User user);
-    void LogIn(User user);
-    void LogOut(User user);
-    void SignUp(User user);
+    User LogIn(User user);
+    User LogOut(User user);
+    User SignUp(User user);
 }
--- SocialNetwork/SocialNetwork.Core/Services/UserService.cs
+++ SocialNetwork/SocialNetwork.Core/Services/UserService.cs
@@ -1,63 +1,57 @@
 ﻿using SocialNetwork.Core.Interfaces;
 using SocialNetwork.Core.Models;
 using System.Text.RegularExpressions;
-using System.Xml.Linq;
 namespace SocialNetwork.Core.Services;
 public class UserService : IUserService
 {
     // TODO repository
-    public List<User> Users {get; set; }
-    public UserService()
+    public IEnumerable<User> GetUsers() 
     {
-        Users = new List<User>();
-    }
-    public List<User> GetUsers() 
-    {
-        return Users;
+        return new List<User>(); // return data from repository
     }
     public User? GetUserById(int id)
     {
-        User? user = Users.FirstOrDefault(unit => unit.Id == id);
+        var user = GetUsers().FirstOrDefault(unit => unit.Id == id);
         return user;
     }
     public User? GetUserByName(string name)
     {
-        User? user = Users.FirstOrDefault(unit => unit.Nickname == name);
+        var user = GetUsers().FirstOrDefault(unit => unit.Nickname == name);
         return user;
     }
     public void UpdateUser(User user)
     {
-        if (!UserIsValid(user))
+        if (!IsUserValid(user))
+            throw new ArgumentException("User credentials aren't valid"); // TODO own types of exceptions
+        var users = GetUsers().ToList();
+        var index = users.IndexOf(users.First(unit => unit.Id == user.Id));
+        users[index] = user;
+    }
+    public User LogIn(User user)
+    {
+        user.IsLoggedIn = true;   // TODO save in repository
+        return user;
+    }
+    public User LogOut(User user)
+    {
+        user.IsLoggedIn = false;   // TODO save in repository
+        return user;
+    }
+    public User SignUp(User user)
+    {
+        if (!IsUserValid(user))
             throw new Exception("User credentials aren't valid"); // TODO own types of exceptions
-        var index = Users.IndexOf(Users.First(unit => unit.Id == user.Id));
-        Users[index] = user;
+        GetUsers().ToList().Add(user);
+        return user;
     }
-    public void LogIn(User user)
-    {
-        user.IsLoggedIn = true;
-    }
-    public void LogOut(User user)
-    {
-        user.IsLoggedIn = false;
-    }
-    public void SignUp(User user)
-    {
-        if (!UserIsValid(user))
-            throw new Exception("User credentials aren't valid"); // TODO own types of exceptions
-        Users.Add(user);
-    }
-    // Validation logic **************************************************************************************************************************
-    private bool UserIsValid(User user)
+    #region Validation logic
+    private bool IsUserValid(User user)
     {
         // Cheks if a string has at least one latin character, at least one digit and only one '_' character. Other characters should be excluded
-        var nick_name_validation = DataIsValid(user.Nickname, @"^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]*_[a-zA-Z\d]*$");
+        var isNicknameValid = new Regex(@"^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]*_[a-zA-Z\d]*$").IsMatch(user.Nickname);
         // Cheks if a string has at least one lower-case latin character, at least one upper-case latin character and at least one digit. The string must be at least 8 characters long
-        var password_validation = DataIsValid(user.Password, @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$");
-        return nick_name_validation && password_validation;
+        var isPasswordValid = new Regex(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$").IsMatch(user.Password);
+        return isNicknameValid && isPasswordValid;
     }
-    private bool DataIsValid(string data, string pattern)
-    {
-        var regex = new Regex(pattern);
-        return regex.IsMatch(data);
-    }
+    #endregion
 }
