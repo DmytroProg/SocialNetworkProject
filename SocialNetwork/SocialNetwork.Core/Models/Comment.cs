0b8b22ad2a179a0e44c51bf50b3cf772f7147052
Scrolix1591 <scrolix36@gmail.com>
2024-11-02 20:16:53 +0100

    Comment model

--- SocialNetwork/SocialNetwork.Core/Models/Comment.cs
+++ SocialNetwork/SocialNetwork.Core/Models/Comment.cs
@@ -7,10 +7,8 @@ namespace SocialNetwork.Core.Models
     {
         [Key]
         public int Id { get; set; }
-        [ForeignKey("Post")]
-        public int PostId { get; set; }
-        [ForeignKey("User")]
-        public int UserId { get; set; }
+        public Post Post { get; set; }
+        public User User { get; set; }
         public string Text { get; set; }
         public DateTime CreatedAt { get; set; }
     }
