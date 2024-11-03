ffe7f0fa8e21f9d71d645ea98da4d0ce589417bb
DevXCoder47 <nbelokrinitskiy@ukr.net>
2024-11-03 18:21:39 +0200

    UserController added. UserService Modified

--- SocialNetwork/SocialNetwork.API/Controllers/UserController.cs
+++ SocialNetwork/SocialNetwork.API/Controllers/UserController.cs
@@ -1,16 +1,111 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SocialNetwork.Core.Interfaces;
 using SocialNetwork.Core.Models;
+using System.Xml.Linq;
 namespace SocialNetwork.API.Controllers;
 [ApiController]
 [Route("users")]
 public class UserController : ControllerBase
 {
-    private readonly IUserService _userService;
+    private readonly IUserService _service;
 
-    public UserController(IUserService userService)
+    public UserController(IUserService service)
     {
-        _userService = userService;
+        _service = service;
     }
-
+    #region Get Methods
+    [HttpGet("all")]
+    public async Task<ActionResult<IEnumerable<User>>> GetAllUsers([FromQuery] int skip = 0, [FromQuery] int take = 10)
+    {
+        try
+        {
+            return Ok( await _service.GetUsers(skip, take));
+        }
+        catch(Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    [HttpGet("id")]
+    public async Task<ActionResult<User>> GetUserbyId([FromRoute] int id)
+    {
+        try
+        {
+            return Ok(await _service.GetUserById(id));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    [HttpGet("name")]
+    public async Task<ActionResult<User>> GetUserbyName([FromRoute] string name)
+    {
+        try
+        {
+            return Ok(await _service.GetUserByName(name));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    #endregion
+    #region Post Methods
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateUser([FromBody]User user)
+    {
+        try
+        {
+            var createdUser = await _service.SignUp(user);
+            return Created(Url.Action(nameof(GetUserbyId), new { id = createdUser.Id }), createdUser);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    #endregion
+    #region Put Methods
+    [HttpPut]
+    public async Task<ActionResult<User>> UpdateUser(int id, User user)
+    {
+        try
+        {
+            var updatedUser = await _service.UpdateUser(id, user);
+            return Created(Url.Action(nameof(GetUserbyId), new { id = updatedUser.Id }), updatedUser);
+        }
+        catch(Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    #endregion
+    #region Patch Methods
+    [HttpPatch("log_in/user")]
+    public async Task<ActionResult<User>> LogInUser([FromRoute]int id)
+    {
+        try
+        {
+            return Ok( await _service.LogIn(id));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    [HttpPatch("log_out/user")]
+    public async Task<ActionResult> LogOutUser([FromRoute] int id)
+    {
+        try
+        {
+            await _service.LogOut(id);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    #endregion
 }
--- SocialNetwork/SocialNetwork.Core/Services/UserService.cs
+++ SocialNetwork/SocialNetwork.Core/Services/UserService.cs
@@ -39,15 +39,21 @@ public class UserService : IUserService
         user.Password = HashManager.HashCreate(user.Password);
         return _repository.Update<User>(user,id);
     }
-    public Task<User> LogIn(User user)
+    public async Task<User> LogIn(int id)
     {
-        user.IsLoggedIn = true;   
-        return _repository.Update<User>(user, user.Id);
+        var targetUser = await _repository.GetById<User>(id);
+        if (targetUser == null)
+            throw new ArgumentException("User not found");
+        targetUser.IsLoggedIn = true;   
+        return await _repository.Update<User>(targetUser, id);
     }
-    public Task LogOut(User user)
+    public async Task LogOut(int id)
     {
-        user.IsLoggedIn = false;   
-        return _repository.Update<User>(user, user.Id);
+        var targetUser = await _repository.GetById<User>(id);
+        if (targetUser == null)
+            throw new ArgumentException("User not found");
+        targetUser.IsLoggedIn = false;
+        await _repository.Update<User>(targetUser, id);
     }
     public Task<User> SignUp(User user)
     {
