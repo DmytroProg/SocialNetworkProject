0b3032a6ac70714d37b587bf3dbfae475e5ea25e
DevXCoder47 <nbelokrinitskiy@ukr.net>
2024-11-11 10:01:11 +0200

    Undesirable files hidden. PostDTO modified

--- SocialNetwork/SocialNetwork.Core/DTOs/PostDTO.cs
+++ SocialNetwork/SocialNetwork.Core/DTOs/PostDTO.cs
@@ -9,6 +9,7 @@ namespace SocialNetwork.Core.DTOs
 {
     public class PostDTO
     {
+        public int Id { get; set; }
         public string? Description { get; set; }
         public string? Media { get; set; }
         public long LikesCount { get; set; }
