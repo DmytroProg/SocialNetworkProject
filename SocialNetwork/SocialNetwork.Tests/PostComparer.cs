3e8ad96cf22098f1c40ac9a010301b8a09cf17bc
DmytroProg <dmytroprog@gmail.com>
2024-11-15 01:37:44 +0200

    PostService tests implemented

---
+++ SocialNetwork/SocialNetwork.Tests/PostComparer.cs
@@ -0,0 +1,20 @@
+﻿using SocialNetwork.Core.Models;
+
+namespace SocialNetwork.Tests;
+
+public class PostComparer : IEqualityComparer<Post>
+{
+    public bool Equals(Post x, Post y)
+    {
+        if (ReferenceEquals(x, y)) return true;
+        if (ReferenceEquals(x, null)) return false;
+        if (ReferenceEquals(y, null)) return false;
+        if (x.GetType() != y.GetType()) return false;
+        return x.Id == y.Id && x.Description == y.Description && x.Media == y.Media && x.LikesCount == y.LikesCount;
+    }
+
+    public int GetHashCode(Post obj)
+    {
+        return HashCode.Combine(obj.Id, obj.Description, obj.Media, obj.LikesCount);
+    }
+}
