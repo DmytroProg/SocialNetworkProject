74e824dabcfe813a2566a7eb17ca630528749ea3
DevXCoder47 <nbelokrinitskiy@ukr.net>
2024-11-26 11:28:39 +0200

    Bug fixed

--- SocialNetwork/SocialNetwork.Core/DTOs/UserDTO.cs
+++ SocialNetwork/SocialNetwork.Core/DTOs/UserDTO.cs
@@ -11,7 +11,6 @@ namespace SocialNetwork.Core.DTOs
     {
         public int Id { get; set; }
         public string Nickname { get; set; }
-        public string Password { get; set; }
         public string? PhoneNumber { get; set; }
         public bool IsLoggedIn { get; set; }
         public string? UserDescription { get; set; }
