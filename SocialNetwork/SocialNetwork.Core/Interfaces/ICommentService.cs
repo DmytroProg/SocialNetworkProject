fe6f150e6fcca2769e72fc44944e2dd9063d9244
DanyilZolotopupov <zolotofm2005@gmail.com>
2024-11-26 12:30:48 -0500

    added a method to ICommentServise and removed async

---
+++ SocialNetwork/SocialNetwork.Core/Interfaces/ICommentService.cs
@@ -0,0 +1,9 @@
+﻿using SocialNetwork.Core.Models;
+namespace SocialNetwork.Core.Interfaces;
+
+internal interface ICommentServise
+{
+	Task<Comment> AddComment(Comment comment);
+	Task<Comment> GetComment(int id);
+	Task<IEnumerable<Comment>> GetComments(int postId);
+}
---
+++ SocialNetwork/SocialNetwork.Core/Services/CommentService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Core.Interfaces;
+using SocialNetwork.Core.Models;
+
+namespace SocialNetwork.Core.Services;
+
+public class CommentService : ICommentServise
+{
+	private readonly IRepository _repository;
+	public CommentService(IRepository repository)
+	{
+		_repository = repository;
+	}
+
+	public Task<Comment> AddComment(Comment comment)
+	{
+		if (comment == null)
+		{
+			throw new ArgumentNullException(nameof(comment), "Comment can't be empty");
+		}
+		return _repository.Add(comment);
+	}
+
+	public async Task<Comment> GetComment(int id)
+	{
+		var comment = await _repository.GetById<Comment>(id);
+
+		if (comment == null)
+		{
+			throw new KeyNotFoundException($"Comment with ID {id} not found.");
+		}
+
+		return comment;
+	}
+
+	public async Task<IEnumerable<Comment>> GetComments(int postId)
+	{
+		var comments = await _repository.GetAll<Comment>().Where(c => c.PostId == postId).ToListAsync();
+
+		if (!comments.Any())
+		{
+			throw new KeyNotFoundException($"No comments found for Post ID {postId}.");
+		}
+
+		return comments;
+	}
+
+}
